0c80dd53a79c6f392a63d48ab86e65ed940ff3b8
Disu Ibukunoluwa <idisu.2201049@stu.cu.edu.ng>
2025-11-14 03:39:53 +0100

    Update download_model.py with direct Google Drive link and add requests to requirements.txt

--- download_model.py
+++ download_model.py
@@ -1,10 +1,7 @@
-# download_model.py
 import requests
 
-url = "https://drive.google.com/file/d/12loWK8x0TFpiatoNqfDTD6BGZPNcgSYN/view?usp=sharing"  
-
+url = "https://drive.google.com/uc?export=download&id=12loWK8x0TFpiatoNqfDTD6BGZPNcgSYN"
 r = requests.get(url, stream=True)
-
 with open("model.h5", "wb") as f:
     for chunk in r.iter_content(chunk_size=8192):
         f.write(chunk)
